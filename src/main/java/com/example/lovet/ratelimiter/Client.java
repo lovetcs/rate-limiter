9206f49cee81c5ddee1f227e6ab04a37dae9f8bd
Lovet <102820462+lovetcs@users.noreply.github.com>
2026-05-19 17:05:40 +0000

    Client class created

---
+++ src/main/java/com/example/lovet/ratelimiter/Client.java
@@ -0,0 +1,24 @@
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GeneationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String emailAddress;
+    private LocalDateTime createdAt;
+
+
+
+}
